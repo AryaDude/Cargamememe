86fdafe0ebe35b58a7d23a20223f9a2696a129f6
AryaDude <76018122+AryaDude@users.noreply.github.com>
2021-04-19 18:54:18 -0400

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,45 @@
+class Form{
+    constructor(){
+        this.title = createElement("h2");
+        this.button = createButton("START");
+        this.greeting = createElement("h3");
+        this.input = createInput("Pinacle Name");
+        this.endGame = createButton("Reset")
+    }
+    hide(){
+        this.input.hide();
+        this.button.hide();
+        this.greeting.hide();
+        this.title.hide();
+    }
+
+    display(){
+        
+        this.title.html("«Pinacle Racers»");
+        this.title.position(width/2 - 20, 100);
+        
+        this.input.position(width/2 - 20, 200);
+        
+        this.button.position(width/2 + 30,250);
+
+        this.endGame.position(width* 0.75 + 60,140);
+        
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            this.title.position(width/2 - 20,100);
+            player.name = this.input.value();
+            playerCount+=1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("Welcome, Pinacle Racer "+ player.name + ", to the pinacle road. Here you will race payers from around the world. Work hard, and rise to the top... of Pinacle Road!");
+            this.greeting.position(width/2 - 20,200);
+        })
+        this.endGame.mousePressed(()=>{
+            player.updateCount(0);
+            game.update(0); 
+            Player.updateEndCars(0)
+        })
+    }
+}
